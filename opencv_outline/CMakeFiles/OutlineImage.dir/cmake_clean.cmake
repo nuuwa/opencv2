8d5a14f487d4660ee6bfa671e882b20f8bd11b5a
Justin Zong <angel@nuuwa.com>
2017-07-03 21:58:21 +0800

    Initial version

---
+++ opencv_outline/CMakeFiles/OutlineImage.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/OutlineImage.dir/OutlineImage.cpp.o"
+  "OutlineImage.pdb"
+  "OutlineImage"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/OutlineImage.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
