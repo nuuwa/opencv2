3375355fef51b11c15c06ab2cec4d2b1707bf669
Justin Zong <angel@nuuwa.com>
2017-07-04 00:48:01 +0800

    generic_corner_detector

---
+++ generic_corner_detector/CMakeFiles/generic_corner_detector.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/generic_corner_detector.dir/generic_corner_detector.cpp.o"
+  "generic_corner_detector.pdb"
+  "generic_corner_detector"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/generic_corner_detector.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
