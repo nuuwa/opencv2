efa6f8029bbbc0ad6677c5227cf7a6b68efe3177
Justin Zong <angel@nuuwa.com>
2017-07-03 23:51:43 +0800

    Add Shitomashi Detector

---
+++ shitomashi_detector/CMakeFiles/shitomashi_detector.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/shitomashi_detector.dir/shitomashi_detector.cpp.o"
+  "shitomashi_detector.pdb"
+  "shitomashi_detector"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/shitomashi_detector.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
